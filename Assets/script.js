34c79d31d8ff55728677ec9e710769fe8ef47c32
Cullen <cullenjcasey@gmail.com>
2022-01-31 00:14:53 -0800

    can edit all event boxes

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,22 @@ const time = document.getElementById("currentDay");
 time.textContent = (moment().format('dddd, MMMM Do'));
 var currentHour = moment().format('HH');
 
+$(".col-10").on("click", "p", function () {
+    var text = $(this).text();
+    var textInput = $("<textarea>").addClass("form-control").val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+$(".col-10").on("blur", "textarea", function () {
+    var taskP = $("<p>").addClass("m-1").text($(this).val() + " ");
+    if (taskP) {
+        $(this).replaceWith(taskP);
+        return;
+    }
+    $(this).replaceWith(space);
+});
+
 const checkTime = function () {
     for (let i = 9; i < 18; i++) {
         console.log(i);
