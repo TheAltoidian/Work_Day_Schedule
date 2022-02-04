05d174ff9b28e278a6e658c773ee417a9d557c2a
Cullen <cullenjcasey@gmail.com>
2022-02-03 19:38:54 -0800

    almost have everything working, need to get jquery working on dynamic content

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,34 @@ const time = document.getElementById("currentDay");
 time.textContent = (moment().format('dddd, MMMM Do'));
 var currentHour = moment().format('HH');
 
+let eventList = [];
+
+// make a list of current events
+const eventListUpdate = function () {
+    for (let i = 9; i < 18; i++) {
+        eventList.push(document.getElementById("event" + i).textContent);
+        console.log(eventList);
+    }
+};
+
+// display saved events upon opening the page
+const fillList = function () {
+    for (let i = 9; i < 18; i++) {
+        let currentEvent = document.getElementById("event" + i);
+        currentEvent.textContent = eventList[i - 9];
+        console.log(eventList[i - 9]);
+        console.log(currentEvent.id);
+    }
+};
+
+// click save to save events
+$(".text-white").on("click", function () {
+    eventList = [];
+    eventListUpdate();
+    localStorage.setItem("day'sEvents", JSON.stringify(eventList));
+});
+
+// change event to editable
 $(".col-10").on("click", "p", function () {
     var text = $(this).text();
     var textInput = $("<textarea>").addClass("form-control").val(text);
@@ -9,6 +37,7 @@ $(".col-10").on("click", "p", function () {
     textInput.trigger("focus");
 });
 
+// stop editing event
 $(".col-10").on("blur", "textarea", function () {
     var taskP = $("<p>").addClass("m-1").text($(this).val() + " ");
     if (taskP) {
@@ -18,10 +47,11 @@ $(".col-10").on("blur", "textarea", function () {
     $(this).replaceWith(space);
 });
 
+// color events based on past or future
 const checkTime = function () {
     for (let i = 9; i < 18; i++) {
-        console.log(i);
-        var currentEvent = document.getElementById("event" + i);
+        // console.log(i);
+        let currentEvent = document.getElementById("event" + i);
         if (i < currentHour) {
             currentEvent.setAttribute("class", "p-3 mb-2 col-10 bg-secondary text-black");
         }
@@ -36,3 +66,5 @@ const checkTime = function () {
 }
 
 checkTime();
+// eventListUpdate();
+fillList();
